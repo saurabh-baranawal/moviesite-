087554a3223da2a3e3e395b6a9201066e439e248
Saurabh baranawal <122673095+saurabh-baranawal@users.noreply.github.com>
2023-02-21 10:45:37 +0530

    Add files via upload

---
+++ script/scifi.js
@@ -0,0 +1,6 @@
+const menuToggle = document.querySelector('.menu-toggle');
+const navMenu = document.querySelector('.nav-menu');
+
+menuToggle.addEventListener('click', () => {
+  navMenu.classList.toggle('active');
+});
